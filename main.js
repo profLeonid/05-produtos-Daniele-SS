fbd819e1376048af41bbcf98575ce7c9230588f4
DANIELE SILVA SANTOS <25203622@dsn127m1242313.cfp127aluno.local>
2026-02-24 11:30:05 -0300

    Adicionado o campo de valore e de quantidade dos produtos

--- main.js
+++ main.js
@@ -3,10 +3,18 @@
 function adicionarProduto(){
     const lista = document.getElementById('lista')
     const produto = document.getElementById('produto')
+    const valor = document.getElementById('valor')
+    const quantidade = document.getElementById('quantidade')
 
-    const span = document.createElement('span')
-    span.textContent = produto.value
-    span.className = 'flex flex-con'
+    if(produto.value == || valor.value == '' || quantidade.value == ''){
+        const span = document.createElement('span')
+        span.textContent = produto.value
+        span.className = 'flex flex-con'
+        lista.appendChild(span)
+    }
 
-    lista.appendChild(span)
+    document.getElementById('produto').value = ''
+    document.getElementById('valor').value = ''
+    document.getElementById('quantidade').value = ''
+
 }
